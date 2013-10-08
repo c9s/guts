1abdb93634e08f6ab779d25ceef1a219c7868a71
c9s <cornelius.howl@gmail.com>
2013-10-08 20:48:08 +0800

    Put lexer struct in another package file.

---
+++ lexer.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+type CoffeeLex struct {
+	s   string
+	pos int
+}
+
+func (l *CoffeeLex) Lex(lval *CoffeeSymType) int {
+	var c rune = ' '
+	for c == ' ' {
+		if l.pos == len(l.s) {
+			return 0
+		}
+		c = rune(l.s[l.pos])
+		l.pos += 1
+	}
+
+	if unicode.IsDigit(c) {
+		lval.val = int(c - '0')
+		return DIGIT
+	} else if unicode.IsLower(c) {
+		lval.val = int(c - 'a')
+		return LETTER
+	}
+	return int(c)
+}
+
+func (l *CoffeeLex) Error(s string) {
+	fmt.Printf("syntax error: %s\n", s)
+}
