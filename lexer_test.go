e4dcecb4c4902019965a84d9278a7a428d44461d
c9s <cornelius.howl@gmail.com>
2013-10-09 16:25:21 +0800

    basic lexer testing skeleton

--- lexer_test.go
+++ lexer_test.go
@@ -3,5 +3,20 @@ package main
 import "testing"
 
 func TestLexer(t *testing.T) {
-
+	input := `
+	a = 1
+	b = 2
+	foo23 = 3
+	`
+	lexer := CoffeeLex{
+		input: input,
+		start: 0,
+		pos:   0,
+		items: make(chan LexItem),
+	}
+	for r := lexer.next(); r != eof; r = lexer.next() {
+		t.Log(r)
+		_ = r
+	}
+	_ = lexer
 }
