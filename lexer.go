ed845186f863739026f3d8fbe7bf3e4611ba8628
c9s <cornelius.howl@gmail.com>
2013-10-09 15:20:16 +0800

    basic lexer methods: peek(), backups(), next()

--- lexer.go
+++ lexer.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"unicode"
+	"unicode/utf8"
 )
 
 type CoffeeLex struct {
@@ -10,10 +12,15 @@ type CoffeeLex struct {
 	input string
 	start int
 	pos   int
+
+	// XXX
+	width int
 	items chan LexItem
 }
 
 type TokenType int
+
+const eof = -1
 
 const (
 	TokenDot = iota
@@ -40,13 +47,75 @@ func (self *LexItem) String() string {
 }
 
 /*
+func lexText(l *CoffeeLex) stateFn {
+    for {
+        if strings.HasPrefix(l.input[l.pos:], leftMeta) {
+            if l.pos > l.start {
+                l.emit(itemText)
+            }
+            return lexLeftMeta    // Next state.
+        }
+        if l.next() == eof { break }
+    }
+    // Correctly reached EOF.
+    if l.pos > l.start {
+        l.emit(itemText)
+    }
+    l.emit(itemEOF)  // Useful to make EOF a token.
+    return nil       // Stop the run loop.
+}
+func (l *CoffeeLex) emit(t TokenType) {
+    l.items <- item{t, l.input[l.start:l.pos]}
+    l.start = l.pos
+}
 func (l *CoffeeLex) run() {
-	for state := l.Lex(); state != nil {
+	for state := l.lexText; state != nil {
 		state = state(l)
 	}
 	close(l.items)
 }
 */
+
+// peek returns but does not consume
+// the next rune in the input.
+func (l *CoffeeLex) peek() rune {
+	rune := l.next()
+	l.backup()
+	return rune
+}
+
+// ignore skips over the pending input before this point.
+func (l *CoffeeLex) ignore() {
+	l.start = l.pos
+}
+
+// accept consumes the next rune
+// if it's from the valid set.
+func (l *CoffeeLex) accept(valid string) bool {
+	if strings.IndexRune(valid, l.next()) >= 0 {
+		return true
+	}
+	l.backup()
+	return false
+}
+
+// backup steps back one rune.
+// Can be called only once per call of next.
+func (l *CoffeeLex) backup() {
+	l.pos -= l.width
+}
+
+// next returns the next rune in the input.
+func (l *CoffeeLex) next() (r rune) {
+	if l.pos >= len(l.input) {
+		l.width = 0
+		return eof
+	}
+	r, l.width =
+		utf8.DecodeRuneInString(l.input[l.pos:])
+	l.pos += l.width
+	return r
+}
 
 // returns a token
 func (l *CoffeeLex) Lex(lval *CoffeeSymType) int {
