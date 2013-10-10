97018cceeb5537debad8c7392e163eb469df5809
c9s <cornelius.howl@gmail.com>
2013-10-10 16:04:38 +0800

    Add empty Node structures

---
+++ node.go
@@ -0,0 +1,24 @@
+package main
+
+type Node struct {
+}
+
+type ExprNode struct {
+	Node
+}
+
+type StatementNode struct {
+	Node
+}
+
+type IfNode struct {
+	Node
+}
+
+type ElseIfNode struct {
+	Node
+}
+
+type ElseNode struct {
+	Node
+}
