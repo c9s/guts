eac6280b835d95be771d1c1e41e28ccbb65afcd6
c9s <cornelius.howl@gmail.com>
2013-10-18 19:59:27 +0800

    Add more ClassStatement AST node methods

--- src/gutscript/ast/class.go
+++ src/gutscript/ast/class.go
@@ -4,8 +4,18 @@ type ClassStatement struct {
 	Name       string
 	Properties []Node
 	Methods    []Node
+	Super      *string
+	Interfaces []string
 }
 
 func CreateClassStatement(className string) Node {
-	return ClassStatement{className, nil, nil}
+	return ClassStatement{className, nil, nil, nil, []string{}}
+}
+
+func (self ClassStatement) SetSuperClass(className string) {
+	self.Super = &className
+}
+
+func (self ClassStatement) AddInterface(intf string) {
+	self.Interfaces = append(self.Interfaces, intf)
 }
